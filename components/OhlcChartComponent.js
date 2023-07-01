efdbd1efef0309fed882b0e0e2f83f9db56cdbdd
aryanteng <aryanteng892@gmail.com>
2023-07-02 01:07:41 +0530

    using useCallback for convert to ohlc

--- components/OhlcChartComponent.js
+++ components/OhlcChartComponent.js
@@ -1,7 +1,6 @@
-import React, { useEffect, useRef } from 'react';
+import React, { useCallback, useEffect, useRef } from 'react';
 import { createChart, ColorType } from 'lightweight-charts';
 import { memo } from 'react';
-import { convertToOHLC } from '../utils/convertToOHLC';
 
 function OhlcChartComponent(props) {
   const {
@@ -17,6 +16,74 @@ function OhlcChartComponent(props) {
   } = props;
 
   const chartContainerRef = useRef();
+
+  const convertToOHLC = useCallback((data, resolution) => {
+    const sortedData = [...data].sort(
+      (a, b) => new Date(a[0]) - new Date(b[0])
+    );
+
+    const ohlcData = [];
+    let currentOpen = 0;
+    let currentHigh = 0;
+    let currentLow = 0;
+    let currentClose = 0;
+    let currentVolume = 0;
+    let currentTimestamp = null;
+
+    sortedData.forEach((item) => {
+      const timestamp = new Date(item[0]);
+      const value = item[1];
+      const volume = item[2];
+
+      if (!currentTimestamp) {
+        currentTimestamp = timestamp;
+        currentOpen = value;
+        currentHigh = value;
+        currentLow = value;
+        currentClose = value;
+        currentVolume = volume;
+      }
+
+      const minutesDiff = Math.floor(
+        (timestamp - currentTimestamp) / (1000 * 60)
+      );
+      if (minutesDiff >= resolution) {
+        ohlcData.push({
+          time: currentTimestamp.getTime(),
+          open: currentOpen,
+          high: currentHigh,
+          low: currentLow,
+          close: currentClose,
+          volume: currentVolume,
+        });
+
+        currentTimestamp = timestamp;
+        currentOpen = value;
+        currentHigh = value;
+        currentLow = value;
+        currentClose = value;
+        currentVolume = volume;
+      } else {
+        currentHigh = Math.max(currentHigh, value);
+        currentLow = Math.min(currentLow, value);
+        currentClose = value;
+        currentVolume += volume;
+      }
+    });
+
+    if (currentTimestamp) {
+      ohlcData.push({
+        time: currentTimestamp.getTime(),
+        open: currentOpen,
+        high: currentHigh,
+        low: currentLow,
+        close: currentClose,
+        volume: currentVolume,
+      });
+    }
+
+    return ohlcData;
+  }, []);
 
   useEffect(() => {
     const handleResize = () => {
@@ -58,6 +125,7 @@ function OhlcChartComponent(props) {
     textColor,
     areaTopColor,
     areaBottomColor,
+    convertToOHLC,
   ]);
 
   return <div className='w-full' ref={chartContainerRef} />;
