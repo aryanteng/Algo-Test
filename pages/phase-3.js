3e7481ec4a49175fc64b90a29162eba718924aec
aryanteng <aryanteng892@gmail.com>
2023-07-02 01:12:53 +0530

    different resolutions

--- pages/phase-3.js
+++ pages/phase-3.js
@@ -7,6 +7,11 @@ function Phase3() {
     <div className='flex flex-col items-center gap-10 p-10'>
       <h1 className='text-5xl'>Phase III: OHLC</h1>
       <OhlcChartComponent data={data1} resolution={1} />
+      <p className='text-xl'>Resolution: 1 minute</p>
+      <OhlcChartComponent data={data1} resolution={5} />
+      <p className='text-xl'>Resolution: 5 minutes</p>
+      <OhlcChartComponent data={data1} resolution={30} />
+      <p className='text-xl'>Resolution: 30 minutes</p>
     </div>
   );
 }
