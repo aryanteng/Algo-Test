27318ccf5497a563d030d3114c6907fd1bc02ddb
aryanteng <aryanteng892@gmail.com>
2023-07-02 15:43:09 +0530

    phase 4 completed

--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,6 @@ export default function Home() {
           Get started by editing{' '}
           <code className={styles.code}>pages/index.js</code>
         </p>
-
         <div className={styles.grid}>
           <a href='https://nextjs.org/docs' className={styles.card}>
             <h2>Documentation &rarr;</h2>
