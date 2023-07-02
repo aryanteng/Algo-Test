26e84c4d1013b77dc68c0cbd7267996321fbbff2
aryanteng <aryanteng892@gmail.com>
2023-07-02 23:30:55 +0530

    dropdown for selecting resolution

--- pages/phase-3.js
+++ pages/phase-3.js
@@ -1,14 +1,26 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { data1 } from '../data/data1';
 import OhlcChartComponent from '../components/OhlcChartComponent';
 
 function Phase3() {
+  const [resolution, setResolution] = useState(1);
+
   return (
     <div className='flex flex-col items-center gap-10 p-10'>
       <h1 className='text-5xl'>Phase III: OHLC</h1>
-      <OhlcChartComponent data={data1} resolution={1} />
-      <OhlcChartComponent data={data1} resolution={5} />
-      <OhlcChartComponent data={data1} resolution={30} />
+      <label htmlFor='resolution'>Resolution:</label>
+      <select
+        id='resolution'
+        value={resolution}
+        onChange={(e) => {
+          setResolution(e.target.value);
+        }}
+      >
+        <option value={1}>1</option>
+        <option value={5}>5</option>
+        <option value={30}>30</option>
+      </select>
+      <OhlcChartComponent data={data1} resolution={resolution} />
     </div>
   );
 }
