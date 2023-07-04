31597541de1a846c6e4f81486350cec88aeb9725
aryanteng <aryanteng892@gmail.com>
2023-07-04 13:42:02 +0530

    combination logic

--- utils/combineDataByTimestamp.js
+++ utils/combineDataByTimestamp.js
@@ -1,25 +1,17 @@
 export const combineDataByTimestamp = (combinedArray) => {
   const combinedMap = new Map();
-  let previousLtp = null;
-
   combinedArray.forEach((item) => {
     const timestamp = item[0];
     const ltp = item[1];
     const volume = item[2];
-
     if (combinedMap.has(timestamp)) {
       const existingItem = combinedMap.get(timestamp);
       existingItem[1] += ltp;
       existingItem[2] += volume;
     } else {
       const newItem = [timestamp, ltp, volume];
-      if (previousLtp !== null) {
-        newItem[1] += previousLtp;
-      }
       combinedMap.set(timestamp, newItem);
     }
-
-    previousLtp = ltp;
   });
 
   const combinedResult = Array.from(combinedMap.values());
